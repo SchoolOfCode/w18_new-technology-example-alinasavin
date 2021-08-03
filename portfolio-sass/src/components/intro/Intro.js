69bb3ffa9b335dcf46cce9e02787f464fdfeb02e
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-08-03 19:51:43 +0100

    intro components and ityped.js to do text animation

--- portfolio-sass/src/components/intro/Intro.js
+++ portfolio-sass/src/components/intro/Intro.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import './intro.scss';
+import { ArrowDownward } from '@material-ui/icons';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
-export default function Intro() {
-  return <div></div>;
-}
+const Intro = () => {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Developer', 'Designer', 'Programmer'],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/assets/IMG_0662.jpg" alt="profile"></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi 👋🏼 There, I'm</h2>
+          <h1>Alina Savin</h1>
+          <h3>
+            Web <span ref={textRef}></span>
+          </h3>
+          <p>
+            I live in the UK and studying full time to become a front-end
+            developer. I have studied Chemistry but I feel like now I'm
+            following my dreams. I can't wait for what the future holds.
+          </p>
+          <a href="#portfolio">
+            <ArrowDownward className="icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
